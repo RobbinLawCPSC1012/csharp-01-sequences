8e80ee1ab936c33ee2a818bdad5c70012d2840ee
Robbin Law <robbinl@telusplanet.net>
2019-09-08 10:38:52 -0600

    Add project files.

---
+++ 01-HelloWorld/01-HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Everyone");
+        }
+    }
+}
