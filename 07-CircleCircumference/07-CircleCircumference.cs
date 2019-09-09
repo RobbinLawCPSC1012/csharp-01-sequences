2bd80bd6ff0accdded96bcb9e16ba6b7c1536d61
robbinl <robbinl@telusplanet.net>
2019-09-09 14:00:53 -0600

    Added Cirlce

---
+++ 07-CircleCircumference/07-CircleCircumference.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07_CircleCircumference
+{
+    class CircleCircumference
+    {
+        static void Main()
+        {
+            double myRadius;
+            double myPi = Math.PI;
+            double myCircumference;
+
+            Console.Write("Enter the Radius as a double: ");
+            myRadius = double.Parse(Console.ReadLine());
+
+            myCircumference = 2.0 * myPi * myRadius;
+
+            Console.WriteLine($"The Radius was {myRadius:n} and its Circumference is {myCircumference:n}");
+        }
+    }
+}
