7f2d37fa8a376489d20f1bc5bc8614aa9216aa58
RobbinL <RobbinL@WB304-99>
2019-09-09 07:26:51 -0600

    Brackets around WriteLine

--- 04-VariablesAndStringBuilding/04-VariablesAndStringBuilding.cs
+++ 04-VariablesAndStringBuilding/04-VariablesAndStringBuilding.cs
@@ -13,14 +13,14 @@ namespace _04_VariablesAndStringBuilding
             bool hasLightBulb = true;
 
             // when a string begins with $ then whenever there is an {} inside is a variable contents,justify format:presentation format
-            Console.WriteLine($"howMany is {howMany,-15:d2} lamps"); //left justified, decimal whole number, 2 digits
-            Console.WriteLine($"howMany is {howMany,15:d3} lamps");  //right justified, decimal whole number, 3 digits
-            Console.WriteLine($"howMany is {howMany,15:n3} lamps");  //right justified, numeric 3 decimals
-            Console.WriteLine($"height is {height,-15:n2} cm");  //left justified, numeric 2 decimals
-            Console.WriteLine($"height is {height,15:n3} cm");  //right justified, numeric 3 decimals
-            Console.WriteLine($"price is {price,-10:c2}");  //left justified, currency 2 decimals
-            Console.WriteLine($"price is {price,10:c3}");  //right justified, currency 3 decimals
-            Console.WriteLine($"price is {price,10:n1}");  //right justified, numeric 1 decimals
+            Console.WriteLine($"howMany is [{howMany,-15:d2}] lamps"); //left justified, decimal whole number, 2 digits
+            Console.WriteLine($"howMany is [{howMany,15:d3}] lamps");  //right justified, decimal whole number, 3 digits
+            Console.WriteLine($"howMany is [{howMany,15:n3}] lamps");  //right justified, numeric 3 decimals
+            Console.WriteLine($"height is [{height,-15:n2}] cm");  //left justified, numeric 2 decimals
+            Console.WriteLine($"height is [{height,15:n3}] cm");  //right justified, numeric 3 decimals
+            Console.WriteLine($"price is [{price,-10:c2}]");  //left justified, currency 2 decimals
+            Console.WriteLine($"price is [{price,10:c3}]");  //right justified, currency 3 decimals
+            Console.WriteLine($"price is [{price,10:n1}]");  //right justified, numeric 1 decimals
             // string building
             string desc =
                 $"\nName: {name} \nPrice: {price:c}" +
