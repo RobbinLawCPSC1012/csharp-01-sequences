f87007952cbf0c23cc1a5274c62a8b45c6a83d90
robbinl <robbinl@telusplanet.net>
2019-09-09 14:59:09 -0600

    3 questions added

---
+++ 09-SimpleFutureValue/SimpleFutureValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// this program is written assuming that interest is compounded monthly or 12 times per year
+namespace _09_SimpleFutureValue
+{
+    class SimpleFutureValue
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Investment Amount:");
+            double investAmt = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter annual interest rate in percentage:");
+            double interestRate = double.Parse(Console.ReadLine());
+            double monthlyInterest = interestRate / 12 / 100;
+
+            Console.WriteLine("Enter Number of years:");
+            int numYears = int.Parse(Console.ReadLine());
+
+            double futureVal = investAmt * Math.Pow(1 + monthlyInterest, numYears * 12);
+
+            Console.WriteLine($"Future Value is {futureVal:C}");
+        }
+    }
+}
---
+++ 10-2018InClass01/2018InClass01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _10_2018InClass01
+{
+    class InClass01
+    {
+        static void Main()
+        {
+            // Declare variables that are Constants
+            const double provTaxRate = 0.06;
+            const double fedTaxRate = 0.05;
+
+            // Inputs
+            double itemPrice;
+            int numItems;
+
+            // Results
+            double beforeTax;
+            double totalProvTax;
+            double totalFedTax;
+            double totalTax;
+            double totalSale;
+
+            Console.WriteLine("Enter the item price as a double: ");
+            itemPrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Number of Items as an integer: ");
+            numItems = int.Parse(Console.ReadLine());
+
+            beforeTax = itemPrice * numItems;
+            Console.WriteLine($"Purchase Total (before Tax): {beforeTax:C}");
+
+            // Calculate Tax
+            totalProvTax = beforeTax * provTaxRate;
+            Console.WriteLine($"Total Provincial Tax: {totalProvTax:C}");
+
+            totalFedTax = beforeTax * fedTaxRate;
+            Console.WriteLine($"Total Federal Tax: {totalFedTax:C}");
+
+            totalTax = totalProvTax + totalFedTax;
+            Console.WriteLine($"Total Tax Amount: {totalTax:C}");
+
+            totalSale = beforeTax + totalTax;
+            Console.WriteLine($"Total Sale price: {totalSale:C}");
+        }
+    }
+}
